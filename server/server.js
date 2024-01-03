3da151ba173bd5ab93d3c9a9a354a01f90adf172
natalie <hardman.natalie85@gmail.com>
2024-01-03 15:34:07 +0000

    update client and server js

--- server/server.js
+++ server/server.js
@@ -4,9 +4,34 @@ import cors from "cors";
 
 const db = new Database("database.db");
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+const port = 8080;
 
 app.get("/", (request, response) => {
   response.json("You are in my root route");
+});
+
+app.get("/customers", function (req, res) {
+  const customers = db.prepare("SELECT * FROM customers").all();
+  res.json(customers);
+  console.log(req.body);
+});
+
+app.get("/search", (req, res) => {
+  const customers = db.prepare("SELECT * FROM customers").all();
+  const searchTerm = req.body.searchTerm.toLowerCase();
+//responds with the filtered stalls list
+  const filteredCustomers = customers.filter((customer) => {
+    return customer.trading_name.toLowerCase().includes(searchTerm) ||
+           customer.categories.toLowerCase().includes(searchTerm) ||
+           customer.description_txt.toLowerCase().includes(searchTerm);
+  });
+//sends just filtered list of stall to the client
+  res.json(filteredCustomers);
+});
+
+
+app.listen(port, () => {
+  console.log("Server is running");
 });
